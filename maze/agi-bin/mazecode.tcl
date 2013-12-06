abb6f1949e296ed9bd8cf2ae876d08e6f8831544
BeF <bef@eventphone.de>
2013-12-06 15:40:12 +0000

    .

--- maze/agi-bin/mazecode.tcl
+++ maze/agi-bin/mazecode.tcl
@@ -1,8 +1,8 @@
 ## generate things out of cid & level
 proc normalize_cid {cid} {
 	regsub -all {[^\d]} $cid {} cid
-	regsub {^0+} $cid {} cid
 	if {[string length $cid] > 8} { set cid [string range $cid end-7 end] }
+	regsub {^0+} $cid {} cid
 	if {$cid == ""} { set cid 0 }
 	return $cid
 }
